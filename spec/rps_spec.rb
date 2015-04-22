b051a9fb6ff54ad14075c972091c268009f3cdbe
Erin & Peter <student@epicodus.com>
2015-04-22 11:20:05 -0700

    second test written and failed

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -7,5 +7,9 @@ describe('String#rps') do
   it('returns true if rock is the object and scissors is the argument') do
     expect("rock".rps("scissors")).to(eq(true))
   end
+  it('returns true if rock is the object and scissors is the argument') do
+    expect("scissors".rps("rock")).to(eq(false))
+  end
+
 
 end

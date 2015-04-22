086c0c10ceb48e23acf6dcd3cd678f97c49d133b
Erin & Peter <student@epicodus.com>
2015-04-22 11:32:20 -0700

    it includes all winning scenarios

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,7 +2,29 @@ class String
   define_method(:rps) do |x|
 
       if (self == "rock" && x == "scissors")
+        puts = "player 1 wins"
+        true
+
+      elsif (self == "scissors" && x == "paper")
+        puts = "player 1 wins"
+        true
+
+      elsif (self == "paper" && x == "rock")
+          puts = "player 1 wins"
           true
-       end
-     end
+
+      elsif (self == "scissors" && x == "rock")
+        puts = "player 2 wins"
+        false
+
+      elsif (self == "rock" && x == "paper")
+        puts = "player 2 wins"
+        false
+
+      elsif (self == "paper" && x == "scissors")
+        puts = "player 2 wins"
+        false
+        end
+
+   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -7,6 +7,7 @@ describe('String#rps') do
   it('returns true if rock is the object and scissors is the argument') do
     expect("rock".rps("scissors")).to(eq(true))
   end
+
   it('returns true if rock is the object and scissors is the argument') do
     expect("scissors".rps("rock")).to(eq(false))
   end
